19182111aeedf4f702b6af64fad7f4183ea947ed
nstrucky <nickstruckmeyer@gmail.com>
2017-01-09 19:29:14 -0500

    Added AsyncTask to download data for listview

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -17,6 +17,7 @@ package com.example.android.quakereport;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -25,29 +26,144 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.List;
 
 public class EarthquakeActivity extends AppCompatActivity {
 
     public static final String LOG_TAG = EarthquakeActivity.class.getName();
+
+    List<Earthquake> earthquakes;
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
 
-        // Create a fake list of earthquake locations.
-        ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes();
 
+        JsonAsyncTask task = new JsonAsyncTask();
+        task.execute();
+
+
+    }
+
+    //    http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&limit=15
+
+
+
+
+    private class JsonAsyncTask extends AsyncTask<Void, Void, String> {
+
+
+
+        @Override
+        protected String doInBackground(Void... params) {
+            HttpURLConnection httpURLConnection = null;
+            InputStream inputStream = null;
+            StringBuilder stringBuilder = new StringBuilder();
+            String jsonString = null;
+            URL url = buidURL();
+
+            try {
+                httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setRequestMethod("GET");
+                httpURLConnection.connect();
+
+                inputStream = httpURLConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(inputStream);
+                BufferedReader buffer = new BufferedReader(reader);
+
+                String line = null;
+                while ((line = buffer.readLine()) != null) {
+
+                    stringBuilder.append(line + "\n");
+                }
+
+                jsonString = stringBuilder.toString();
+
+                Log.i(LOG_TAG, jsonString);
+
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "IOException", e);
+            } finally {
+                if (httpURLConnection != null) {
+                    httpURLConnection.disconnect();
+                }
+
+                if (inputStream != null) {
+                    try {
+                        inputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            return jsonString;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            earthquakes = QueryUtils.extractEarthquakes(s);
+            setListData();
+
+
+        }
+
+        private URL buidURL() {
+            URL url = null;
+            final String BASE_URL = "http://earthquake.usgs.gov/fdsnws/event/1/query?";
+            final String PARAM_FORMAT = "format";
+            final String PARAM_LIMIT = "limit";
+            String limit = "15";
+            String format = "geojson";
+
+            Uri uri = Uri.parse(BASE_URL).buildUpon()
+                    .appendQueryParameter(PARAM_FORMAT, format)
+                    .appendQueryParameter(PARAM_LIMIT, limit)
+                    .build();
+
+            try {
+                url = new URL(uri.toString());
+
+            } catch (MalformedURLException e) {
+                Log.e(LOG_TAG, "MalformedURL", e);
+            }
+
+            return url;
+
+        }
+    }
+
+
+
+    protected void setListData() {
         // Find a reference to the {@link ListView} in the layout
         final ListView earthquakeListView = (ListView) findViewById(R.id.list);
 
         // Create a new {@link ArrayAdapter} of earthquakes
 
         final EarthquakeArrayAdapter adapter =
-                new EarthquakeArrayAdapter(this, earthquakes);
+                new EarthquakeArrayAdapter(getApplicationContext(), earthquakes);
 
         // Set the adapter on the {@link ListView}
         // so the list can be populated in the user interface
@@ -64,4 +180,7 @@ public class EarthquakeActivity extends AppCompatActivity {
             }
         });
     }
+
+
+
 }
--- app/src/main/java/com/example/android/quakereport/QueryUtils.java
+++ app/src/main/java/com/example/android/quakereport/QueryUtils.java
@@ -17,6 +17,8 @@ import java.util.Date;
 
 public final class QueryUtils {
 
+
+
     /** Sample JSON response for a USGS query */
     private static final String SAMPLE_JSON_RESPONSE = "{\"type\":\"FeatureCollection\",\"metadata\":{\"generated\":1462295443000,\"url\":\"http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2016-01-01&endtime=2016-01-31&minmag=6&limit=10\",\"title\":\"USGS Earthquakes\",\"status\":200,\"api\":\"1.5.2\",\"limit\":10,\"offset\":1,\"count\":10},\"features\":[{\"type\":\"Feature\",\"properties\":{\"mag\":7.2,\"place\":\"88km N of Yelizovo, Russia\",\"time\":1454124312220,\"updated\":1460674294040,\"tz\":720,\"url\":\"http://earthquake.usgs.gov/earthquakes/eventpage/us20004vvx\",\"detail\":\"http://earthquake.usgs.gov/fdsnws/event/1/query?eventid=us20004vvx&format=geojson\",\"felt\":2,\"cdi\":3.4,\"mmi\":5.82,\"alert\":\"green\",\"status\":\"reviewed\",\"tsunami\":1,\"sig\":798,\"net\":\"us\",\"code\":\"20004vvx\",\"ids\":\",at00o1qxho,pt16030050,us20004vvx,gcmt20160130032510,\",\"sources\":\",at,pt,us,gcmt,\",\"types\":\",cap,dyfi,finite-fault,general-link,general-text,geoserve,impact-link,impact-text,losspager,moment-tensor,nearby-cities,origin,phase-data,shakemap,tectonic-summary,\",\"nst\":null,\"dmin\":0.958,\"rms\":1.19,\"gap\":17,\"magType\":\"mww\",\"type\":\"earthquake\",\"title\":\"M 7.2 - 88km N of Yelizovo, Russia\"},\"geometry\":{\"type\":\"Point\",\"coordinates\":[158.5463,53.9776,177]},\"id\":\"us20004vvx\"},\n" +
             "{\"type\":\"Feature\",\"properties\":{\"mag\":6.1,\"place\":\"94km SSE of Taron, Papua New Guinea\",\"time\":1453777820750,\"updated\":1460156775040,\"tz\":600,\"url\":\"http://earthquake.usgs.gov/earthquakes/eventpage/us20004uks\",\"detail\":\"http://earthquake.usgs.gov/fdsnws/event/1/query?eventid=us20004uks&format=geojson\",\"felt\":null,\"cdi\":null,\"mmi\":4.1,\"alert\":\"green\",\"status\":\"reviewed\",\"tsunami\":1,\"sig\":572,\"net\":\"us\",\"code\":\"20004uks\",\"ids\":\",us20004uks,gcmt20160126031023,\",\"sources\":\",us,gcmt,\",\"types\":\",cap,geoserve,losspager,moment-tensor,nearby-cities,origin,phase-data,shakemap,tectonic-summary,\",\"nst\":null,\"dmin\":1.537,\"rms\":0.74,\"gap\":25,\"magType\":\"mww\",\"type\":\"earthquake\",\"title\":\"M 6.1 - 94km SSE of Taron, Papua New Guinea\"},\"geometry\":{\"type\":\"Point\",\"coordinates\":[153.2454,-5.2952,26]},\"id\":\"us20004uks\"},\n" +
@@ -41,7 +43,7 @@ public final class QueryUtils {
      * Return a list of {@link Earthquake} objects that has been built up from
      * parsing a JSON response.
      */
-    public static ArrayList<Earthquake> extractEarthquakes() {
+    public static ArrayList<Earthquake> extractEarthquakes(String jsonToParse) {
 
         // Create an empty ArrayList that we can start adding earthquakes to
         ArrayList<Earthquake> earthquakes = new ArrayList<>();
@@ -50,7 +52,7 @@ public final class QueryUtils {
         // is formatted, a JSONException exception object will be thrown.
         // Catch the exception so the app doesn't crash, and print the error message to the logs.
         try {
-            JSONObject jsonObject = new JSONObject(SAMPLE_JSON_RESPONSE);
+            JSONObject jsonObject = new JSONObject(jsonToParse);
             JSONArray earthquakesAsFeatures = jsonObject.getJSONArray("features");
 
             for (int i = 0; i < earthquakesAsFeatures.length(); i++) {
